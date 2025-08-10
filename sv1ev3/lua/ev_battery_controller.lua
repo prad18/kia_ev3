7b8d926e430c7bcb51119128e3770a1ae28dd127
Pradish Vaidya S <64153555+prad18@users.noreply.github.com>
2025-08-10 19:13:00 +0530

    feat: add battery degradation params

--- sv1ev3/lua/ev_battery_controller.lua
+++ sv1ev3/lua/ev_battery_controller.lua
@@ -4,6 +4,12 @@ local M = {}
 function M.init(jbeamData)
   log('I','EVBATT','init() ran')               -- visible in console
   electrics.values.evbatt_heartbeat = 0        -- numeric counter we can read from Python
+
+  -- read battery parameters provided in JBeam via v.data
+  M.capFadeCoeff    = v.data.cap_fade_coeff    or 0.0
+  M.resGrowthCoeff  = v.data.res_growth_coeff  or 0.0
+  M.ocvTempCoeff    = v.data.ocv_temp_coeff    or 0.0
+  M.thermalResCoeff = v.data.thermal_res_coeff or 0.0
 end
 
 function M.updateGFX(dt)
@@ -13,8 +19,10 @@ function M.updateGFX(dt)
   e.batteryVoltage = 371
   e.batteryCurrent = 12
   e.packPower = 4.5
-  e.packTemp = 26.3
-  e.soh = 0.99
+  e.packTemp = 26.3 + M.thermalResCoeff
+  e.soh = 1 - M.capFadeCoeff
+  e.resGrowthCoeff = M.resGrowthCoeff
+  e.ocvTempCoeff = M.ocvTempCoeff
   e.fuel = e.soc
   e.fuel_capacity = 81.4
   e.fuel_volume = e.fuel_capacity * e.soc
